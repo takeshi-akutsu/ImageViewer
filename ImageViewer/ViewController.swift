3c4f59a99181994d66ae0c529a62cbd41947e700
Takeshi Akutsu <takeshi.akutsu0303+github@gmail.com>
2020-04-16 17:20:29 +0900

    Initial Commit

---
+++ ImageViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageViewer
+//
+//  Created by 阿久津　岳志 on 2020/04/16.
+//  Copyright © 2020 阿久津　岳志. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
